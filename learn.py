f1b226f6e8d42e541339ebf64701fd6be0a4ffa9
Andy Chen <andy.chen121@imperial.ac.uk>
2023-10-17 15:05:39 +0100

    basic structure

---
+++ learn.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+cleanData = np.loadtxt("wifi_db/clean_dataset.txt")
+noisyDate = np.loadtxt("wifi_db/noisy_dataset.txt")
+
+class Node:
+    def __init__(self, attr, val, left, right):
+        self.attr = attr
+        self.val = val
+        self.left = left
+        self.right = right
+
+
