b79ec279528d9a324f73a9a76f71f1e9c6007ec9
Andy Chen <andy.chen121@imperial.ac.uk>
2023-10-23 21:35:59 +0100

    finished correct way find split dynamic dataset

--- decision_tree.py
+++ decision_tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+from model import ValueInfo, OptimumFinder
 
 # read dataset to np_array
 cleanData = np.loadtxt("wifi_db/clean_dataset.txt")
@@ -36,39 +37,87 @@ def count_labels(allData):
 label_count = count_labels(cleanData)
 
 
-def sortColumn(dataset, attrNum):
+def sortColumn(dataset, attr):
     # sort the entire dataset on one attribute
-    sortedDataset = dataset[dataset[:, attrNum].argsort()]
+    sortedDataset = dataset[dataset[:, attr].argsort()]
     return sortedDataset
 
 
 def find_optimal_split_point(dataset):
-    max_IG = -1  # This is the Information gained
+    min_remainder = None  # This is the Information gained
     max_IG_attr = -1  # This is the attribute where the max IG value belongs
-    max_IG_split_value = -1  # This is the split value where the max IG value appeared at
+    max_IG_split = -1  # This is the split value where the max IG value appeared at
     for column in range(attr_count):
         sorted_dataset = sortColumn(dataset, column)  # sort the dataset
         label_frequency = [0 for i in range(label_count)]
-        row_value = None
+
+        value = None
+        value_group = []
+        split_points = []
         for row in sorted_dataset:
             label = int(row[-1])
+            v = row[column]
             label_frequency[label - 1] += 1  # calculate the frequency of this label
-            if not row_value:
-                row_value = row[column]
-            elif row_value != row[column]:
-                row_value = row[column]
+            if v == value:
+                info = value_group[-1][1]
+                info.frequency[label-1] += 1
+                info.count += 1
+            else:
+                info = ValueInfo(label_count)
+                info.frequency[label-1] = 1
+                info.count = 1
+                if len(value_group) >= 2:
+                    info2 = value_group[-2][1]
+                    info1 = value_group[-1][1]
+                    if info2.unique_label() < 0 or info2.unique_label() != info1.unique_label():
+                        split_points.append(v)
+                value_group.append((v, info))
+                value = v
+        if len(value_group) >= 2:
+            info2 = value_group[-2][1]
+            info1 = value_group[-1][1]
+            if info2.unique_label() < 0 or info2.unique_label() != info1.unique_label():
+                split_points.append(v)
 
-    #TODO: Complete the function
+        left_finder = OptimumFinder(0, [0 for i in range(label_count)])
+        right_finder = OptimumFinder(len(sorted_dataset), label_frequency)
+        if min_remainder is None:
+            min_remainder = right_finder.entropy()
+        
+        i = 0
+        while len(split_points) > 0:
+            (value, info) = value_group[i]
+            if value < split_points[0]:
+                left_finder.update(info, 1)
+                right_finder.update(info, -1)
+                i += 1
+            else:
+                split = split_points.pop(0)
+                rem = (left_finder.entropy()*left_finder.size + right_finder.entropy()*right_finder.size) / len(sorted_dataset)
+                # print(column, split, rem)
+                if rem < min_remainder:
+                    min_remainder = rem
+                    max_IG_attr = column
+                    max_IG_split = split
+    return max_IG_attr, max_IG_split
 
+def split_dataset(dataset, attr, split):#Get the left and right datasets from a splitting point
+        left_set=dataset[dataset[:,attr] < split]
+        right_set=dataset[dataset[:,attr] >= split]
+        return np.array(left_set), np.array(right_set)
 
 def decision_tree_learning(dataset, depth):
-    allLabels = dataset[:, -1]  # Get the column with all the labels
-    if len(allLabels) == 0:
-        return Node(None, 999, None, None), 999
-    if np.all(allLabels == allLabels[0]) or depth >= 100:  # Checks if all of the labels are the same
-        return Node(None, allLabels[0], None, None), depth
-    # TODO: Find optimal spliting point, Store spliting point as node, Split the data into 2 datasets, repeat until
-    #  reach depth
+    print(depth)
+    all_labels = dataset[:, -1]  # Get the column with all the labels
+    if np.all(all_labels == all_labels[0]) or depth >= 100:  # Checks if all of the labels are the same
+        return Node(None, all_labels[0], None, None), depth
+    else:
+        attr, split = find_optimal_split_point(dataset)
+        split_node = Node(attr, split, None, None)
+        ldata, rdata = split_dataset(dataset, attr, split)
+        split_node.left, ldepth = decision_tree_learning(ldata, depth+1)
+        split_node.right, rdepth = decision_tree_learning(rdata, depth+1)
+        return split_node, max(ldepth, rdepth)
 
 
 def fit(dataset, depth):
@@ -83,3 +132,7 @@ def predict():
 def cross_validation():
     #TODO:
     return
+
+# print(find_optimal_split_point(cleanData))
+tree, depth = decision_tree_learning(cleanData, 0)
+print(tree.show(), "depth", depth)
---
+++ model.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class ValueInfo:
+    # storing the frequency count of each label appears in one value, as well as its entropy
+    # "value" takes the form of x1 = 61, 60, 75 etc. equivalent to "sunny", "windy" in tennis example
+    def __init__(self, labelCount, count=0, entropy=0):
+        # label count is 4 in this dataset, room 1,2,3,4 
+        self.frequency = [0 for i in range(labelCount)]
+        self.count = count
+        self.unique = None
+
+    def unique_label(self):
+        if self.unique != None:
+            return self.unique
+        num_distinct = 0
+        label = -1
+        for i in range(len(self.frequency)):
+            if self.frequency[i] > 0:
+                num_distinct += 1
+                label = i+1
+            if num_distinct > 1:
+                self.unique = -1
+                return self.unique
+        self.unique = label
+        return self.unique
+            
+class OptimumFinder:
+    def __init__(self, size, prefix_freq):
+        self.size = size
+        self.prefix_freq = prefix_freq
+
+    def entropy(self):
+        ent = 0
+        for pf in self.prefix_freq:
+            if pf > 0:
+                prob = pf/self.size
+                ent -= prob * np.log2(prob)
+        return ent
+
+    def update(self, info, fac):
+        for i in range(len(self.prefix_freq)):
+            self.prefix_freq[i] += fac*info.frequency[i]
+        self.size += fac * info.count
+
+
