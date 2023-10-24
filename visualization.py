914e3edc1fc70d66ef641a508ac182045e64c3d3
rl2121 <rl2121@ic.ac.uk>
2023-10-24 17:18:42 +0100

    Completed visualization.py with subtrees method

--- visualization.py
+++ visualization.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from decision_tree import Node, decision_tree_learning
+from decision_tree import decision_tree_learning
+
 
 class Tree_Visualizer:
 
-    def __init__(self, tree):
-        self.tree = tree
+    def __init__(self, my_tree):
+        self.tree = my_tree
         self.subtree_list = []
 
     def plot_tree(self, node, level=0, position=0, parent_coords=None):
@@ -16,21 +17,22 @@ class Tree_Visualizer:
             if parent_coords:
                 plt.plot([parent_coords[0], node.x], [parent_coords[1], node.y], '-',
                          color=color[level % len(color)])
-            if level < 0:
+            if level < 1 and (node.attr is not None):
+                print (node.attr)
                 self.subtree_list.append(node)
                 plt.text(node.x, node.y, f"subT{len(self.subtree_list)}", ha='center',
                          bbox={"boxstyle": "round", "pad": 0.4, "facecolor": "w", "edgecolor": "k"}, fontsize="small")
             else:
-                if node.attr:
+                if node.attr is not None:
                     plt.text(node.x, node.y, f"attr: {node.attr}\n <{node.val}", ha='center',
                              bbox={"boxstyle": "round", "pad": 0.4, "facecolor": "w", "edgecolor": "k"},
                              fontsize="small")
                 else:
-                    plt.text(node.x, node.y, f" pred: {node.val}", ha='center',
+                    plt.text(node.x, node.y, f" Prediction:\n {node.val}", ha='center',
                              bbox={"boxstyle": "round", "pad": 0.4, "facecolor": "w", "edgecolor": "k"},
                              fontsize="small")
-                self.plot_tree(node.left, level - 1, position - 2 ** level / 15, (node.x, node.y))
-                self.plot_tree(node.right, level - 1, position + 2 ** level / 15, (node.x, node.y))
+                self.plot_tree(node.left, level - 1, position - 2 ** level / 10, (node.x, node.y))
+                self.plot_tree(node.right, level - 1, position + 2 ** level / 10, (node.x, node.y))
 
     def visualize(self):
         # Configure plot
@@ -40,12 +42,15 @@ class Tree_Visualizer:
         self.plot_tree(self.tree, level=5, position=0)
         plt.savefig("mainT.png")
         for index, t in enumerate(self.subtree_list):
-            self.plot_tree(t,level=5, position=0)
-            plt.savefig(f"subT{index}.png")
+            fig, ax = plt.subplots(figsize=(30, 30))
+            plt.axis('off')
+            ax.set_aspect('equal')
+            self.plot_tree(t, level=5, position=0)
+            plt.savefig(f"subT{index+1}.png")
+
 
 if __name__ == '__main__':
     cleanData = np.loadtxt("wifi_db/clean_dataset.txt")
     tree, depth = decision_tree_learning(cleanData, 0)
     visualizer = Tree_Visualizer(tree)
     visualizer.visualize()
-
